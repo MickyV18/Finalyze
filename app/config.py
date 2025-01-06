56961c949d3d6b16458739a7f6e0afd5909c410f
MickyV18 <mickyvalentino18@gmail.com>
2025-01-06 13:25:33 +0700

    railway

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 class Settings:
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-    CALLBACK_URL = os.getenv("CALLBACK_URL")
+    CALLBACK_URL = os.getenv("CALLBACK_URL", "https://finalyze.up.railway.app/auth/callback")
 
 settings = Settings()
 
